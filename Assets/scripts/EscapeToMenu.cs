6d0335eb79900191fae53268d9ac2ff1e305eeeb
Джамшедбек Эминджонов <eminjomovjamik@mail.ru>
2025-12-15 01:07:10 +0300

    version for Kira

--- Assets/scripts/EscapeToMenu.cs
+++ Assets/scripts/EscapeToMenu.cs
@@ -15,8 +15,33 @@ public class EscapeToMenu : MonoBehaviour
         }
 	else if (Input.GetKeyDown("2"))
         {
+            SceneManager.LoadScene("216", LoadSceneMode.Single);
+        }
+else if (Input.GetKeyDown("3"))
+        {
             SceneManager.LoadScene("218", LoadSceneMode.Single);
         }
+else if (Input.GetKeyDown("4"))
+        {
+            SceneManager.LoadScene("256", LoadSceneMode.Single);
+        }
+else if (Input.GetKeyDown("5"))
+        {
+            SceneManager.LoadScene("257", LoadSceneMode.Single);
+        }
+else if (Input.GetKeyDown("6"))
+        {
+            SceneManager.LoadScene("257а", LoadSceneMode.Single);
+        }
+else if (Input.GetKeyDown("7"))
+        {
+            SceneManager.LoadScene("258", LoadSceneMode.Single);
+        }
+else if (Input.GetKeyDown("8"))
+        {
+            SceneManager.LoadScene("260", LoadSceneMode.Single);
+        }
+
 
 
     }
